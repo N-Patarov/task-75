0652a25bd51a13aca097a75dff94cef7c598fa4d
niki <nikolayp1707@gmail.com>
2021-10-30 10:48:11 +0300

    initial

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [save, dontSave] = useState(true);
+  function saveLocal(note){
+    localStorage.setItem("text", document.querySelector(".textarea").value);
+    console.log(localStorage);
+  }
+  function clearText(note){
+    document.querySelector(".textarea").value = "";
+    localStorage.removeItem("text");
+  }
   return (
     <div className="App">
       <div className="box">
         <div className="field">
           <div className="control">
             <textarea className="textarea is-large" placeholder="Notes..." />
+            <script>let text = document.getElementById("textarea is-large").value</script>
           </div>
         </div>
-        <button className="button is-large is-primary is-active">Save</button>
-        <button className="button is-large">Clear</button>
+        <button className="button is-large is-primary is-active"
+        onClick={() => saveLocal()}
+        >Save</button>
+        <button className="button is-large" onClick={() => clearText()}>Clear</button>
       </div>
     </div>
   );
